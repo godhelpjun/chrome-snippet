fa7e18a7df0aefeb5a3aaec6960c20ab035ea923
Rex Pan <rexpan@users.noreply.github.com>
2017-05-03 07:51:39 +0700

    Update download-egghead.js

--- download-egghead.js
+++ download-egghead.js
@@ -1,17 +1,11 @@
-/*
-
-This script copy to clipboard a list of command to download egghead.io videos using youtube-dl.
-
-*/
-
 ;(() => { "use strict";
     
 console.clear();
 copy(
-    Array.from(document.querySelectorAll(".cell-lesson-title .title a"))
+    Array.from(document.querySelectorAll(`.bg-base-secondary.flex.items-center a.pointer[href^="https://egghead.io/lessons/"]`))
     .map((a, i) => ({label: `${(i+1).toString().padStart(2, "0")} ${a.text.replace(/[/\\?%*:|"<>]/g, '_')}`, href:a.href}))
-    .map(x => `youtube-dl -o "${x.label}.%(ext)s" ${x.href}`)
-    .join("\n")
+    .map(x => `youtube-dl -o "${x.label}.%(ext)s" ${x.href}\n`)
+    .join("")
 );
 
 })();
